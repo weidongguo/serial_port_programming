eeec767626217583143078edf39fea79954bc9b5
Weidong Guo <weidongguo@live.com>
2016-02-06 14:42:02 -0800

    can transfer a file byte by byte right now, with a delay of 100ms for every byte. This can be adjusted

--- LINUX/main.cpp
+++ LINUX/main.cpp
@@ -10,35 +10,49 @@
 	#define DPRINT(...) do{}while(0)
 #endif
 
+/* void transferFileSerialPort(int file_fd, int port_num, int ms) 
+ * @brief			transfer a file byte by byte through a serial port
+ * @param[in]	int file_fd 	-file descriptor for the file to be transfered
+ * 						int port_num	-port_num that is used by the RS232 library
+ 														 it actually it's an key for a file descriptor
+														 pointing to the port(e.g. /dev/ttyACM0)
+ *						int ms				-delay between byte 
+ * @return		none
+ *						
+ */
+void transferFileToSerialPort(int file_fd, int port_num, int ms){
+	char c;
+	while( read(file_fd, &c, 1) == 1 ){
+		RS232_SendByte(port_num, c);			
+		printf("%c", c); fflush(stdout); //may need to comment this out, since sys.call add delay too
+		usleep(ms*1000); //may need to add delay here, will test that
+	}
+	printf("End of file transfer\n");
+}
+
+
 int main(int argc, char* argv[]){
 	//const char port[] = "/dev/ttyACM0";
 	char mode[] = "8N1";	
 	int baud_rate = 115200;
 	int port_num;
-	unsigned char buf[4096];
+	int file_fd;
 
-	printf("%d\n", argc);
-	
+	if(argc != 3){
+		printf("Invalid command line argument\ne.g. sudo ./a.out /dev/ttyACM0 filename\n");	
+		return 0;
+	}
 	if( (port_num = openSerialPort(argv[1], baud_rate, mode) ) == -1){;
 		printf("fail to open serial port\n");
 		return 0;
 	}
-
-	char c = '2';	
-	while(1){
-		int n = RS232_PollComport(port_num, buf, sizeof(buf)-1);
-		if( n > 0 ){ //read something
-			buf[n] = '\0'; //mark end of string		
-			printf("Received %d bytes: %s\n", n, buf);
-		}
-		usleep(1000000); //sleep for 1sec
-		RS232_SendByte(port_num, c);
-		usleep(1000);
-		c++; 
-		if(c>='5')
-			c = '2';
-		
+	
+	if( (file_fd = open(argv[2], O_RDWR)) == -1 ){	
+		printf("Invalid filename\n");
+		return 0;
 	}
+	
+	transferFileToSerialPort(file_fd, port_num, 100);			
 
 	return 0;
 }
